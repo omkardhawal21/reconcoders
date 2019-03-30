2a1340350aebef368859c1807f4ba451113d4be3
Prasad Deshmukh <deshmukhprasad@users.noreply.github.com>
2019-03-30 13:43:40 +0530

    Created Models

--- bit/models.py
+++ bit/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+class login(models.Model):
+	username = models.CharField(max_length=200)
+	password = models.CharField(max_length = 200)
+	def __str__(self):
+        return self.username
+
+class Patient(models.Model):		#model for patient
+	patient_name = models.CharField(max_length = 200)
+	patient_address = models.CharField(max_length = 200)
+	bdate = models.DateField()
+	patient_number = models.IntegerFiled(max_length=10)
+	patient_gender = models.CharField(max_length=1)
+	patient_username = models.CharField(max_length = 200)
+	patient_pass = models.CharField(max_length = 200)
+	def __str__(self):
+        return self.patient_name
+
+class Doctor(models.Model):			#model for doctor
+	doctor_name = models.CharField(max_length = 200)
+	doctor_address = models.CharField(max_length = 200)
+	doctor_bdate = models.DateField()
+	doctor_number = models.IntegerFiled(max_length=10)
+	doctor_gender = models.CharField(max_length=1)
+	doctor_username = models.CharField(max_length=200)
+	doctor_pass = models.CharField(max_length=200)
+	def __str__(self):
+	    return self.doctor_name
+
+class Disease(models.Model):		#model for diseases
+	patient_name = models.ForeignKey(Patient ,on_delete=models.CASCADE)
+	symptoms_disease = models.CharField(max_length=200)
+	disease_possible = models.CharField(max_length = 200)
+	medicine = models.CharField(max_length=200)
+	date = models.DateField()
+	def __str__(self):
+        return self.disease_possible
+
+
